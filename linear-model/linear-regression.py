c7ab219d2f6e11b60e2b812a27bfb7710664bd23
duyphuongnguyen1999 <duyphuongnguyen1999@gmail.com>
2024-04-22 21:04:12 +0700

    Initial commit

---
+++ linear-model/linear-regression.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+class LinearRegression_:
+    """
+    Parameters:
+        optimizer (str):
+            Specify optimization method for linear regression
+                - 'ols': Ordinary Least Square
+                - 'gd': Gradient Descent
+                - 'sgd': Stochastics Gradient Descent
+                - 'mgd': Mini-Batch Gradient Descent
+    
+    Attributes:
+    
+    
+    Methods:
+    
+    
+
+    """
+    
+    def __init___(self, ):
+        pass
+    
+    def  
+    
